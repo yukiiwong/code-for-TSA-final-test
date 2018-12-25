2fe2ba0b6e9377d5cc260353f0e00ad5968fe27b
a_kkk_service <38659683+yukiiwong@users.noreply.github.com>
2018-12-25 22:46:33 +0800

    Update caculator.py

--- caculator.py
+++ caculator.py
@@ -1,6 +1,7 @@
 import numpy as np
-from sympy import *
-import matplotlib.pyplot as plt
+#from sympy import *
+#import matplotlib.pyplot as plt
+
 
 x = np.arange(1, 5000, 1)
 #x = symbols('x') #求导时使用
@@ -36,4 +37,26 @@ for i in range(len(x)):
         y2_point = z[i]
 
 print(x_point, y1_point)
+
+####增量分配
+N = 5 #迭代次数
+assign = 3000 #分配流量
+x_a = 1
+x_b = 2
+flow_a = 0
+flow_b = 0
+for i in range(N):
+    if x_a <= x_b:
+        flow_a += assign / N
+        x_a = (1 + ( flow_a / 1000 ) ** 2)
+        print(flow_a)
+        print('x_a = {}'.format(x_a))
+    else:
+        flow_b += assign / N
+        x_b = 2 * ( 1 + ( flow_b / 1000 )** 2)
+        print(flow_b)
+        print('x_b = {}'.format(x_b))
+
+print(x_a,x_b)
+        
 
